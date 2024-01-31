f4ab657b9e3af1f2d0e6a4bab6970781efaecadb
Ali Yacoubi <ali.yacoubi@polymtl.ca>
2024-01-31 15:26:26 -0500

    Tout les tests Shape fonctionnent

--- src/main/java/Shape/Ellipse.java
+++ src/main/java/Shape/Ellipse.java
@@ -11,7 +11,18 @@ public class Ellipse extends BaseShape {
      * @param heightDiameter Height of the Ellipse
      */
     public Ellipse(Double widthDiameter, Double heightDiameter) {
-
+        Double radiusWidth = widthDiameter / 2;
+        Double radiusHeight = heightDiameter / 2;
+        for (Double i = 0.0; i < Math.ceil(widthDiameter); i += 0.5) {
+            for (Double j = 0.0; j < Math.ceil(heightDiameter); j += 0.5) {
+                Double x = -radiusWidth + i;
+                Double y = -radiusHeight + j;
+                if ((x * x) / (radiusWidth * radiusWidth) + (y * y) / (radiusHeight * radiusHeight) <= 1){
+                    Point2d p = new Point2d(x, y);
+                    this.add(p);
+                }
+            }
+        }
     }
 
     /** TODO
@@ -19,7 +30,20 @@ public class Ellipse extends BaseShape {
      * @param dimensions 2D point containing the width and height of the Ellipse
      */
     public Ellipse(Point2d dimensions) {
-
+        Double widthDiameter = dimensions.X();
+        Double heightDiameter = dimensions.Y();
+        Double radiusWidth = widthDiameter / 2;
+        Double radiusHeight = heightDiameter / 2;
+        for (Double i = 0.0; i < Math.ceil(widthDiameter); i += 0.5) {
+            for (Double j = 0.0; j < Math.ceil(heightDiameter); j += 0.5) {
+                Double x = -radiusWidth + i;
+                Double y = -radiusHeight + j;
+                if ((x * x) / (radiusWidth * radiusWidth) + (y * y) / (radiusHeight * radiusHeight) <= 1){
+                    Point2d p = new Point2d(x, y);
+                    this.add(p);
+                }
+            }
+        }
     }
 
     /**
@@ -27,7 +51,51 @@ public class Ellipse extends BaseShape {
      * @param coords Collection of 2D points
      */
     private Ellipse(Collection<Point2d> coords) {
+        BaseShape tempBaseShape = new BaseShape(coords);
+        Double minX = tempBaseShape.getMinX();
+        Double maxX = tempBaseShape.getMaxX();
+        Double minY = tempBaseShape.getMinY();
+        Double maxY = tempBaseShape.getMaxY();
+        Double widthDiameter = 0.0;
+        Double heightDiameter = 0.0;
+        Double radiusWidth = widthDiameter / 2;
+        Double radiusHeight = heightDiameter / 2;
 
+        if (Math.abs(maxX) > Math.abs(minX)){
+            widthDiameter = maxX;
+            if (minX < 0.0){
+                widthDiameter += Math.abs(minX);
+            }
+        }
+        else {
+            widthDiameter = Math.abs(minX);
+            if (maxX > 0.0){
+                widthDiameter += maxX;
+            }
+        }
+        if (Math.abs(maxY) > Math.abs(minY)){
+            heightDiameter = maxY;
+            if (minY < 0.0){
+                heightDiameter += Math.abs(minY);
+            }
+        }
+        else {
+            heightDiameter = Math.abs(minY);
+            if (maxY > 0.0){
+                widthDiameter += maxY;
+            }
+        }
+
+        for (Double i = 0.0; i < Math.ceil(widthDiameter); i += 0.5) {
+            for (Double j = 0.0; j < Math.ceil(heightDiameter); j += 0.5) {
+                Double x = i - radiusWidth;
+                Double y = j - radiusHeight;
+                if ((x * x) / (radiusWidth * radiusWidth) + (y * y) / (radiusHeight * radiusHeight) <= 1){
+                    Point2d p = new Point2d(x, y);
+                    this.add(p);
+                }
+            }
+        }
     }
 
     /** TODO
@@ -35,6 +103,6 @@ public class Ellipse extends BaseShape {
      */
     @Override
     public Ellipse clone() {
-        return null;
+        return new Ellipse(this.cloneCoords());
     }
 }
